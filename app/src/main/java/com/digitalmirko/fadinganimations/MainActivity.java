51b35a43af5e9f621849e63225122d59733a2cc7
Mirko Cukich <mirkocukich@gmail.com>
2020-01-03 21:26:31 -0600

    imageview fade out to fade in code added

--- app/src/main/java/com/digitalmirko/fadinganimations/MainActivity.java
+++ app/src/main/java/com/digitalmirko/fadinganimations/MainActivity.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void fade(View view){
 
         Log.i("Info","Imageview has been tapped");
+
+        // Bart imageview
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+
+        // fade out, 0 will fade out completely, over 2 seconds which is 2000 miliseconds
+        bartImageView.animate().alpha(0).setDuration(2000);
+
+        // Homer imageview
+        ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+
+        // fade in, 1 will fade in completely, over 2 seconds which is 2000 miliseconds
+        homerImageView.animate().alpha(1).setDuration(2000);
     }
 
     @Override
